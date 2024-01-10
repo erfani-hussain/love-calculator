322a3c206f9071a299e3abea8eb2e353013be95c
Hussain Erfani <145542696+erfani-hussain@users.noreply.github.com>
2024-01-10 16:53:07 +0100

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 print("The Love Calculator is calculating your score...")
 name1 = input("Enter your name: ") # What is your name?
 name2 = input("Enter their name: ") # What is their name?
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
 
+# combine names
 combined_names = name1 + name2
+# convert into lowercase
 lower_names = combined_names.lower()
 
+# first digit
 t = lower_names.count("t")
 r = lower_names.count("r")
 u = lower_names.count("u")
 e = lower_names.count("e")
 first_digit = t + r + u + e
 
+# second digit
 l = lower_names.count("l")
 o = lower_names.count("o")
 v = lower_names.count("v")
 e = lower_names.count("e")
 second_digit = l + o + v + e
 
+# concatenation
 score = int(str(first_digit) + str(second_digit))
+
+# if statements
 if (score < 10)  or (score > 90):
   print(f"Your score is {score}, you go together like coke and mentos.")
 elif (score >= 40) and (score <= 50):
